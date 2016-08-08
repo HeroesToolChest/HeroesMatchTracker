fc2a22c782b0deef3c3c1f6d02bc0d20e521e18b
Kevin Oliva <kevin.k.oliva@gmail.com>
2016-08-08 13:16:42 -0500

    Added custom games

---
+++ HeroesParserData/ViewModels/Match/CustomGameViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Heroes.ReplayParser;
+using HeroesParserData.DataQueries.ReplayData;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+namespace HeroesParserData.ViewModels.Match
+{
+    public class CustomGameViewModel : MatchContext
+    {
+        public CustomGameViewModel()
+            : base()
+        {
+
+        }
+
+        protected override async Task RefreshExecute()
+        {
+            await QueryMatchList();
+        }
+
+        private async Task QueryMatchList()
+        {
+            MatchList = new ObservableCollection<Models.DbModels.Replay>(await Query.Replay.ReadGameModeRecordsAsync(GameMode.Custom));
+        }
+    }
+}
