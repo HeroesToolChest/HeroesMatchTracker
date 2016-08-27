cfe20a354a9bce1cd7caf46e488246f79f20aaac
Kevin Oliva <kevin.k.oliva@gmail.com>
2016-08-27 16:02:01 -0500

    Added last match control

---
+++ HeroesParserData/ViewModels/Match/LastMatchViewModel.cs
@@ -0,0 +1,21 @@
+﻿using HeroesParserData.DataQueries.ReplayData;
+using System.Threading.Tasks;
+
+namespace HeroesParserData.ViewModels.Match
+{
+    public class LastMatchViewModel : MatchContext
+    {
+        public LastMatchViewModel()
+            :base()
+        {
+
+        }
+
+        protected override async Task RefreshExecute()
+        {
+            var replay = await Query.Replay.ReadLatestRecordsAsync(1);
+            if (replay[0] != null)
+                await QuerySummaryDetails(replay[0].ReplayId);
+        }
+    }
+}
