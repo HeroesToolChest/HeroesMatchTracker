db0abade98721a1a8f108922f1a3dcccfcff7412
Kevin Oliva <kevin.k.oliva@gmail.com>
2016-10-30 16:10:11 -0500

    Added new game mode tab - Brawl

---
+++ HeroesParserData/ViewModels/Match/BrawlViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Heroes.ReplayParser;
+using HeroesParserData.DataQueries;
+using System.Collections.ObjectModel;
+
+namespace HeroesParserData.ViewModels.Match
+{
+    public class BrawlViewModel : MatchContext
+    {
+        public BrawlViewModel()
+            :base()
+        {
+            HasObservers = false;
+            HasBans = false;
+        }
+
+        protected override void RefreshExecute()
+        {
+            QueryMatchList();
+        }
+
+        private void QueryMatchList()
+        {
+            MatchList = new ObservableCollection<Models.DbModels.Replay>(Query.Replay.ReadGameModeRecords(GameMode.Brawl, GetSelectedSeason));
+            RowsReturned = MatchList.Count;
+        }
+    }
+}
