2d3813e3ee2382aaa41d0336a2faeebc6fb41e5c
Kevin Oliva <kevin.k.oliva@gmail.com>
2016-11-29 17:37:53 -0600

    Check for failed released notes and added usable links to Release Notes window

--- HeroesParserData/ViewModels/ReleaseNotesViewModel.cs
+++ HeroesParserData/ViewModels/ReleaseNotesViewModel.cs
@@ -22,6 +22,22 @@ namespace HeroesParserData.ViewModels
         {
             var listOfReleases = Query.ReleaseNotes.ReadAllRecords().OrderByDescending(x => x.DateReleased).ToList();
 
+            if (listOfReleases.Count <= 0)
+                return;
+
+            Version currentVersion = HPDVersion.GetVersion();
+            if (new Version(listOfReleases[0].Version) < currentVersion)
+            {
+                if (listOfReleases[0].PreRelease)
+                    ReleaseNotesMarkdown += $"##[Pre-release] Heroes Parser Data {currentVersion} (Unknown) {Environment.NewLine}";
+                else
+                    ReleaseNotesMarkdown += $"##Heroes Parser Data {currentVersion} (Unknown) {Environment.NewLine}";
+
+                ReleaseNotesMarkdown += "WARNING - Could not download the latest releases logs. Visit the [releases](https://github.com/koliva8245/HeroesParserData/releases) page for release notes.";
+                ReleaseNotesMarkdown += $"{Environment.NewLine} *** {Environment.NewLine}";
+            }
+
+
             foreach (var release in listOfReleases)
             {
                 if (release.PreRelease)
--- HeroesParserData/Views/ReleaseNotesWindow.xaml.cs
+++ HeroesParserData/Views/ReleaseNotesWindow.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Diagnostics;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace HeroesParserData.Views
 {
@@ -22,6 +11,8 @@ namespace HeroesParserData.Views
         public ReleaseNotesWindow()
         {
             InitializeComponent();
+
+            CommandBindings.Add(new CommandBinding(NavigationCommands.GoToPage, (sender, e) => Process.Start((string)e.Parameter)));
         }
     }
 }
