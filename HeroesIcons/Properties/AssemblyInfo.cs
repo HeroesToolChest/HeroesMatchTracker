f0017eecdfd42085e9eb8ada3dfeaa440239f960
Kevin Oliva <kevin.k.oliva@gmail.com>
2017-01-05 12:27:26 -0600

    Version 1.4.2

--- HeroesIcons/Properties/AssemblyInfo.cs
+++ HeroesIcons/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("HeroesIcons")]
-[assembly: AssemblyCopyright("Copyright © 2016 Kevin Oliva")]
+[assembly: AssemblyCopyright("Copyright © 2016 - 2017 Kevin Oliva")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("4168d60d-d0eb-40a2-9304-d38f14106a48")]
 
 // revision number will be the latest supported build
-[assembly: AssemblyVersion("1.4.0.49008")]
-[assembly: AssemblyFileVersion("1.4.0.49008")]
+[assembly: AssemblyVersion("1.4.2.49076")]
+[assembly: AssemblyFileVersion("1.4.2.49076")]
