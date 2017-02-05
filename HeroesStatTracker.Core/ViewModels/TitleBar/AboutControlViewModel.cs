b571e3ce74147216003f23d609cc30fc7560877b
Kevin Oliva <kevin.k.oliva@gmail.com>
2017-02-05 01:02:37 -0600

    Uncommented assembly versions

--- HeroesStatTracker.Core/ViewModels/TitleBar/AboutControlViewModel.cs
+++ HeroesStatTracker.Core/ViewModels/TitleBar/AboutControlViewModel.cs
@@ -6,10 +6,8 @@ namespace HeroesStatTracker.Core.ViewModels.TitleBar
     {
         public string HeroesStatTrackerCoreVersion { get { return AssemblyVersions.HeroesStatTrackerCoreVersion().ToString(); } }
         public string HeroesStatTrackerDataVersion { get { return AssemblyVersions.HeroesStatTrackerDataVersion().ToString(); } }
-
-        // public string HeroesStatTrackerHelpersVersion { get { return AssemblyVersions.HeroesHelpersVersion().ToString(); } }
+        public string HeroesStatTrackerHelpersVersion { get { return AssemblyVersions.HeroesHelpersVersion().ToString(); } }
         public string HeroesIconsVersion { get { return AssemblyVersions.HeroesIconsVersion().ToString(); } }
-
-        // public string HeroesReplayParserVersion { get { return AssemblyVersions.HeroesReplayParserVersion().ToString(); } }
+        public string HeroesReplayParserVersion { get { return AssemblyVersions.HeroesReplayParserVersion().ToString(); } }
     }
 }
