5fcfc4dcb9587b0d82d4f6516596d6793035904a
Kevin Oliva <kevin.k.oliva@gmail.com>
2017-04-23 18:17:19 -0500

    nlog log file fix

--- HeroesMatchTracker.Core/Updater/AutoUpdater.cs
+++ HeroesMatchTracker.Core/Updater/AutoUpdater.cs
@@ -4,8 +4,6 @@ using HeroesMatchTracker.Data;
 using NLog;
 using Squirrel;
 using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Threading.Tasks;
 
 namespace HeroesMatchTracker.Core.Updater
