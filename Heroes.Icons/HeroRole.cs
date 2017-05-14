25e0dbbfd60abddf48003624e4b021f3af7718b4
Kevin Oliva <kevin.k.oliva@gmail.com>
2017-05-14 18:03:27 -0500

    Fixed up flags

--- Heroes.Icons/HeroRole.cs
+++ Heroes.Icons/HeroRole.cs
@@ -5,11 +5,11 @@ namespace Heroes.Icons
     [Flags]
     public enum HeroRole
     {
-        Unknown = -1,
-        Multiclass = 0,
-        Warrior = 1 << 0,
-        Assassin = 1 << 1,
-        Support = 1 << 2,
-        Specialist = 1 << 3,
+        Unknown = 0,
+        Multiclass = 1 << 0,
+        Warrior = 1 << 1,
+        Assassin = 1 << 2,
+        Support = 1 << 3,
+        Specialist = 1 << 4,
     }
 }
