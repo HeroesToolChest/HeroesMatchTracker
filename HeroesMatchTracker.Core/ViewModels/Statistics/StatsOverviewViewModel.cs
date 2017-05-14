50decf3b79832aea394ff9fa86245abeaf6ff0a5
Kevin Oliva <kevin.k.oliva@gmail.com>
2017-05-13 20:40:04 -0500

    Added hero stats

--- HeroesMatchTracker.Core/ViewModels/Statistics/StatsOverviewViewModel.cs
+++ HeroesMatchTracker.Core/ViewModels/Statistics/StatsOverviewViewModel.cs
@@ -8,6 +8,10 @@ using GalaSoft.MvvmLight.CommandWpf;
 using Heroes.Helpers;
 using System.Collections.ObjectModel;
 using HeroesMatchTracker.Core.Models.StatisticsModels;
+using HeroesMatchTracker.Core.ViewServices;
+using NLog;
+using Heroes.ReplayParser;
+using System.Windows;
 
 namespace HeroesMatchTracker.Core.ViewModels.Statistics
 {
@@ -21,15 +25,22 @@ namespace HeroesMatchTracker.Core.ViewModels.Statistics
         private bool _isTeamLeagueSelected;
         private bool _isCustomGameSelected;
         private bool _isBrawlSelected;
+        private bool _isHeroStatPercentageDataGridVisible;
+        private bool _isHeroStatDataGridVisible;
         private string _selectedSeason;
         private string _selectedHeroStat;
 
+        private ILoadingOverlayWindowService LoadingOverlayWindow;
+
         private ObservableCollection<StatsOverviewHeroes> _heroStatsCollection = new ObservableCollection<StatsOverviewHeroes>();
+        private ObservableCollection<StatsOverviewHeroes> _heroStatsPercentageCollection = new ObservableCollection<StatsOverviewHeroes>();
         private ObservableCollection<StatsOverviewMaps> _mapsStatsCollection = new ObservableCollection<StatsOverviewMaps>();
 
-        public StatsOverviewViewModel(IInternalService internalService)
+        public StatsOverviewViewModel(IInternalService internalService, ILoadingOverlayWindowService loadingOverlayWindow)
             : base(internalService)
         {
+            LoadingOverlayWindow = loadingOverlayWindow;
+
             SeasonList.Add(InitialSeasonListOption);
             SeasonList.Add("Lifetime");
             SeasonList.AddRange(HeroesHelpers.Seasons.GetSeasonList());
@@ -37,9 +48,13 @@ namespace HeroesMatchTracker.Core.ViewModels.Statistics
 
             HeroStatsList.AddRange(HeroesHelpers.OverviewHeroStatOptions.GetOverviewHeroStatOptionList());
             SelectedHeroStat = HeroStatsList[0];
+
+            IsHeroStatPercentageDataGridVisible = false;
+            IsHeroStatDataGridVisible = false;
         }
 
         public RelayCommand QueryOverviewStatsCommand => new RelayCommand(async () => await QueryOverviewStatsAsyncCommand());
+        public RelayCommand QuerySelectedHeroStatCommand => new RelayCommand(async () => await QuerySelectedHeroStatAsyncCommand());
 
         public List<string> SeasonList { get; private set; } = new List<string>();
         public List<string> HeroStatsList { get; private set; } = new List<string>();
@@ -63,7 +78,6 @@ namespace HeroesMatchTracker.Core.ViewModels.Statistics
                 RaisePropertyChanged();
             }
         }
-
 
         public bool IsQuickMatchSelected
         {
@@ -125,6 +139,26 @@ namespace HeroesMatchTracker.Core.ViewModels.Statistics
             }
         }
 
+        public bool IsHeroStatPercentageDataGridVisible
+        {
+            get => _isHeroStatPercentageDataGridVisible;
+            set
+            {
+                _isHeroStatPercentageDataGridVisible = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public bool IsHeroStatDataGridVisible
+        {
+            get => _isHeroStatDataGridVisible;
+            set
+            {
+                _isHeroStatDataGridVisible = value;
+                RaisePropertyChanged();
+            }
+        }
+
         public ObservableCollection<StatsOverviewHeroes> HeroStatsCollection
         {
             get => _heroStatsCollection;
@@ -145,9 +179,192 @@ namespace HeroesMatchTracker.Core.ViewModels.Statistics
             }
         }
 
+        public ObservableCollection<StatsOverviewHeroes> HeroStatsPercentageCollection
+        {
+            get => _heroStatsPercentageCollection;
+            set
+            {
+                _heroStatsPercentageCollection = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        // for query button
         private async Task QueryOverviewStatsAsyncCommand()
         {
-            throw new NotImplementedException();
+            await Task.Run(async () =>
+            {
+                try
+                {
+                    var query = QueryOverviewStatsAsync();
+                    LoadingOverlayWindow.ShowLoadingOverlay();
+
+                    await query;
+                }
+                catch (Exception ex)
+                {
+                    ExceptionLog.Log(LogLevel.Error, ex);
+                    throw;
+                }
+            });
+
+            LoadingOverlayWindow.CloseLoadingOverlay();
+        }
+
+        // just for the Hero Stats datagrid
+        private async Task QuerySelectedHeroStatAsyncCommand()
+        {
+            await Task.Run(async () =>
+            {
+                try
+                {
+                    var query = QuerySelectedHeroStatAsync();
+                    LoadingOverlayWindow.ShowLoadingOverlay();
+
+                    await query;
+                }
+                catch (Exception ex)
+                {
+                    ExceptionLog.Log(LogLevel.Error, ex);
+                    throw;
+                }
+            });
+
+            LoadingOverlayWindow.CloseLoadingOverlay();
+        }
+
+        private async Task QueryOverviewStatsAsync()
+        {
+            Clear();
+
+            if (SelectedSeason == InitialSeasonListOption || string.IsNullOrEmpty(SelectedSeason))
+                return;
+
+            HeroesIcons.LoadLatestHeroesBuild();
+
+            Season selectedSeason = HeroesHelpers.EnumParser.ConvertSeasonStringToEnum(SelectedSeason);
+            OverviewHeroStatOption selectedStatOption = HeroesHelpers.EnumParser.ConvertHeroStatOptionToEnum(SelectedHeroStat);
+
+            GameMode selectedGameModes = SetSelectedGameModes();
+
+            await SetHeroStats(selectedSeason, selectedGameModes, selectedStatOption);
+        }
+
+        private async Task QuerySelectedHeroStatAsync()
+        {
+            ClearHeroStatGridOnly();
+
+            if (SelectedSeason == InitialSeasonListOption || string.IsNullOrEmpty(SelectedSeason))
+                return;
+
+            HeroesIcons.LoadLatestHeroesBuild();
+
+            Season selectedSeason = HeroesHelpers.EnumParser.ConvertSeasonStringToEnum(SelectedSeason);
+            OverviewHeroStatOption selectedStatOption = HeroesHelpers.EnumParser.ConvertHeroStatOptionToEnum(SelectedHeroStat);
+
+            GameMode selectedGameModes = SetSelectedGameModes();
+
+            await SetHeroStats(selectedSeason, selectedGameModes, selectedStatOption);
+        }
+
+        private async Task SetHeroStats(Season season, GameMode gameModes, OverviewHeroStatOption statOption)
+        {
+            var heroesList = HeroesIcons.Heroes().GetListOfHeroes();
+            var heroStatPercentageCollection = new Collection<StatsOverviewHeroes>();
+            var heroStatCollection = new Collection<StatsOverviewHeroes>();
+
+            foreach (var hero in heroesList)
+            {
+                if (statOption == OverviewHeroStatOption.HighestWinRate)
+                {
+                    int wins = Database.ReplaysDb().Statistics.ReadGameResults(hero, season, gameModes, true);
+                    int losses = Database.ReplaysDb().Statistics.ReadGameResults(hero, season, gameModes, false);
+                    int total = wins + losses;
+
+                    StatsOverviewHeroes statsOverviewHeroes = new StatsOverviewHeroes()
+                    {
+                        HeroName = hero,
+                        Value = wins / (double)total,
+                    };
+
+                    IsHeroStatPercentageDataGridVisible = true;
+                    heroStatPercentageCollection.Add(statsOverviewHeroes);
+                }
+                else if (statOption == OverviewHeroStatOption.MostWins)
+                {
+                    int wins = Database.ReplaysDb().Statistics.ReadGameResults(hero, season, gameModes, true);
+
+                    StatsOverviewHeroes statsOverviewHeroes = new StatsOverviewHeroes()
+                    {
+                        HeroName = hero,
+                        Value = wins,
+                    };
+
+                    IsHeroStatDataGridVisible = true;
+                    heroStatCollection.Add(statsOverviewHeroes);
+                }
+                else if (statOption == OverviewHeroStatOption.MostDeaths || statOption == OverviewHeroStatOption.MostKills || statOption == OverviewHeroStatOption.MostAssists)
+                {
+                    int value = Database.ReplaysDb().Statistics.ReadStatResult(hero, season, gameModes, statOption);
+
+                    StatsOverviewHeroes statsOverviewHeroes = new StatsOverviewHeroes()
+                    {
+                        HeroName = hero,
+                        Value = value,
+                    };
+
+                    IsHeroStatDataGridVisible = true;
+                    heroStatCollection.Add(statsOverviewHeroes);
+                }
+            }
+
+            // sort it, then add to ObservableCollection
+            if (IsHeroStatPercentageDataGridVisible)
+                await Application.Current.Dispatcher.InvokeAsync(() => { HeroStatsPercentageCollection = new ObservableCollection<StatsOverviewHeroes>(heroStatPercentageCollection.OrderByDescending(x => x.Value)); });
+
+            if (IsHeroStatDataGridVisible)
+                await Application.Current.Dispatcher.InvokeAsync(() => { HeroStatsCollection = new ObservableCollection<StatsOverviewHeroes>(heroStatCollection.OrderByDescending(x => x.Value)); });
+        }
+
+        private GameMode SetSelectedGameModes()
+        {
+            GameMode gameModes = GameMode.Unknown;
+
+            if (!IsQuickMatchSelected && !IsUnrankedDraftSelected && !IsHeroLeagueSelected && !IsTeamLeagueSelected && !IsCustomGameSelected && !IsBrawlSelected)
+            {
+                gameModes = GameMode.QuickMatch | GameMode.UnrankedDraft | GameMode.HeroLeague | GameMode.TeamLeague;
+            }
+            else
+            {
+                if (IsQuickMatchSelected)
+                    gameModes |= GameMode.QuickMatch;
+                if (IsUnrankedDraftSelected)
+                    gameModes |= GameMode.UnrankedDraft;
+                if (IsHeroLeagueSelected)
+                    gameModes |= GameMode.HeroLeague;
+                if (IsTeamLeagueSelected)
+                    gameModes |= GameMode.TeamLeague;
+                if (IsCustomGameSelected)
+                    gameModes |= GameMode.Custom;
+                if (IsBrawlSelected)
+                    gameModes |= GameMode.Brawl;
+            }
+
+            return gameModes;
+        }
+
+        private void Clear()
+        {
+            ClearHeroStatGridOnly();
+        }
+
+        private void ClearHeroStatGridOnly()
+        {
+            IsHeroStatPercentageDataGridVisible = false;
+            IsHeroStatDataGridVisible = false;
+
+            HeroStatsCollection = null;
+            HeroStatsPercentageCollection = null;
         }
     }
 }
