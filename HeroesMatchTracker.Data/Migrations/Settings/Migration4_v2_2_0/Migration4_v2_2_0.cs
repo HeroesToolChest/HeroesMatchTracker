d743013e819ea4a5de9957dcb5eed0bb7520e447
Kevin Oliva <kevin.k.oliva@gmail.com>
2017-05-27 01:11:27 -0500

    Added some usersettings

---
+++ HeroesMatchTracker.Data/Migrations/Settings/Migration4_v2_2_0/Migration4_v2_2_0.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace HeroesMatchTracker.Data.Migrations.Settings
+{
+    internal class Migration4_v2_2_0 : IMigrationCommand
+    {
+        private int Version = 4;
+
+        public void Command(Dictionary<int, List<string>> migrations, Dictionary<int, List<IMigrationAddon>> migrationAddons)
+        {
+            List<string> steps = new List<string>
+            {
+                @"INSERT INTO UserSettings(Name, Value) VALUES ('ShowToasterUpdateNotification', 'True')",
+                @"INSERT INTO UserSettings(Name, Value) VALUES ('IsUpdateAvailableKnown', 'False')",
+            };
+            migrations.Add(Version, steps);
+
+            List<IMigrationAddon> addonSteps = new List<IMigrationAddon>();
+            migrationAddons.Add(Version, addonSteps);
+        }
+    }
+}
--- HeroesMatchTracker.Data/Migrations/Settings/SettingsContextMigrator.cs
+++ HeroesMatchTracker.Data/Migrations/Settings/SettingsContextMigrator.cs
@@ -8,6 +8,7 @@
             IMigrationList.Add(new Migration1_v1_3_0());
             IMigrationList.Add(new Migration2_v2_0_0());
             IMigrationList.Add(new Migration3_v2_1_0());
+            IMigrationList.Add(new Migration4_v2_2_0());
 
             ExecuteMigrationCommands();
         }
