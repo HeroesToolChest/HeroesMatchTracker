abc5f9a3615e725ffdabde1638c6ad77601bc1d0
Kevin Oliva <kevin.k.oliva@gmail.com>
2017-07-11 19:54:11 -0500

    Upped versions

--- HeroesMatchTracker.Core/Properties/AssemblyInfo.cs
+++ HeroesMatchTracker.Core/Properties/AssemblyInfo.cs
@@ -21,5 +21,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("89afa05a-40d2-4ba0-bf72-6d920177f7ae")]
 
-[assembly: AssemblyVersion("1.2.3")]
-[assembly: AssemblyFileVersion("1.2.3")]
+[assembly: AssemblyVersion("1.3.0")]
+[assembly: AssemblyFileVersion("1.3.0")]
