e2c06ed4c1b255a6785a0589ae686d03c24fa26a
Kevin Oliva <kevin.k.oliva@gmail.com>
2017-09-30 00:59:40 -0500

    Added test for Builds

---
+++ Heroes.Helpers.Tests/BuildsTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Heroes.Helpers.Tests
+{
+    [TestClass]
+    public class BuildsTests
+    {
+        [TestMethod]
+        public void GetReplayBuildsFromSeasonTest()
+        {
+            foreach (Season season in Enum.GetValues(typeof(Season)))
+            {
+                Assert.IsNotNull(HeroesHelpers.Builds.GetReplayBuildsFromSeason(season));
+            }
+        }
+    }
+}
