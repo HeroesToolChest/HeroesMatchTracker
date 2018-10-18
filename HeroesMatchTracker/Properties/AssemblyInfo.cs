26ef084fca16410ec665d178bbcf039401d1b7d1
Kevin Oliva <kevin.k.oliva@gmail.com>
2018-10-17 20:54:37 -0500

    Version 2.11.0

--- HeroesMatchTracker/Properties/AssemblyInfo.cs
+++ HeroesMatchTracker/Properties/AssemblyInfo.cs
@@ -24,5 +24,5 @@ using System.Windows;
     ResourceDictionaryLocation.SourceAssembly)]
 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.10.3")]
-[assembly: AssemblyFileVersion("2.10.3")]
+[assembly: AssemblyVersion("2.11.0")]
+[assembly: AssemblyFileVersion("2.11.0")]
