a30381ea4d92f1d1b94e86edcb1529c636aeeed9
Kevin Oliva <kevin.k.oliva@gmail.com>
2021-01-10 16:21:04 -0600

    Set initial count

--- HeroesMatchTracker.Infrastructure/ReplayParser/ReplayParseData.cs
+++ HeroesMatchTracker.Infrastructure/ReplayParser/ReplayParseData.cs
@@ -304,7 +304,7 @@ namespace HeroesMatchTracker.Infrastructure.ReplayParser
             if (draftPicks.Count < 1)
                 return;
 
-            replayMatch.ReplayMatchDraftPicks = new List<ReplayMatchDraftPick>();
+            replayMatch.ReplayMatchDraftPicks = new List<ReplayMatchDraftPick>(draftPicks.Count);
 
             for (int i = 0; i < draftPicks.Count; i++)
             {
